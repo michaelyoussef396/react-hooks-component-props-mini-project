b1eddab453af5e80e866980e5fa7fbf0b7e090a9
michael youssef <michaelyoussef396@gmail.com>
2024-01-10 16:09:17 +1100

    plog site up with bonus

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,18 @@ import React from "react";
 import blogData from "../data/blog";
 import Header from "./Header";
 import About from "./About";
-import ArticleList from "./ArticleList";
+import ArticleList from "./AcrticleList";
 
 console.log(blogData);
 
 function App() {
-    return (
-        <div className="App">
-            <Header name={blogData.name} />
-            <About image={blogData.image} about={blogData.about} />
-            <ArticleList posts={blogData.posts}/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Header name={blogData.name} />
+      <About image={blogData.image} about={blogData.about} />
+      <ArticleList posts={blogData.posts} />
+    </div>
+  );
 }
 
 export default App;
